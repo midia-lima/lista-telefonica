110cdd7ccdb4227bafd6345ed9be59c7b4706253
midia-lima <midialp2@gmail.com>
2021-08-05 07:05:35 -0300

    atualizando

--- js/app.module.js
+++ js/app.module.js
@@ -1,2 +1,13 @@
 //módulo
-angular.module('listaTelefonica', ["ngMessages", "serialGenerator"])
+angular.module('listaTelefonica', ["ngMessages", "ngRoute", "serialGenerator"])
+.config(function ($routeProvider){
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/contato', {
+        templateUrl: 'view/contato.html',
+        controller: 'ListaTelefonicaCtrl'
+       
+    });
+
+})
